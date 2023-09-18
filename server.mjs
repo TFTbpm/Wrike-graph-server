47bc925f11e0ef825307d409d57b9d94ea944b7e
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-09-18 16:09:42 -0500

    added index.html to path

--- server.mjs
+++ server.mjs
@@ -4,34 +4,31 @@ import cors from "cors";
 import "dotenv/config";
 const app = express();
 
-const client_id = process.env.VITE_client_id;
-const redirect_uri = process.env.VITE_redirect_uri;
-const graphScope = ["https://graph.microsoft.com/.default", "offline_access"];
-const wrikeScope = ["Default"];
-const salesSiteID = process.env.VITE_salesSiteID;
-const customerListID = process.env.VITE_customerListID;
-const rfqListID = process.env.VITE_rfqListID;
-const wrikeSalesSpaceID = process.env.VITE_wrikeSalesSpaceID;
-const tenantID = process.env.VITE_tenantID;
-const clientID = process.env.VITE_clientID;
-const graphRedirectURI = process.env.VITE_graphRedirectURI;
-
 app.use(cors("*"));
 
 app.use(express.json());
 
-// app.use(express.static(path.join("./", "public")));
-
 app.post("/wrike", (req, res) => {
-  // req.body.json().then((data) => {
-  //   console.log(data);
-  // });
   console.log(req.body);
   res.status(200).send("good");
 });
+
+app.use(express.static(path.join("./", "public")));
 
 app.listen(5501, () => {
   console.log("running server");
 });
 
 app.listen();
+
+// const client_id = process.env.VITE_client_id;
+// const redirect_uri = process.env.VITE_redirect_uri;
+// const graphScope = ["https://graph.microsoft.com/.default", "offline_access"];
+// const wrikeScope = ["Default"];
+// const salesSiteID = process.env.VITE_salesSiteID;
+// const customerListID = process.env.VITE_customerListID;
+// const rfqListID = process.env.VITE_rfqListID;
+// const wrikeSalesSpaceID = process.env.VITE_wrikeSalesSpaceID;
+// const tenantID = process.env.VITE_tenantID;
+// const clientID = process.env.VITE_clientID;
+// const graphRedirectURI = process.env.VITE_graphRedirectURI;
