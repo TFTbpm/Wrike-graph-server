bf21651339b3befabca4b5f58f8d0ff64e481f4c
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-09-19 14:05:34 -0500

    Cleaned up code

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require("express");
-// import express from "express";
-// import cors from "cors";
-// import "dotenv/config";
-const app = express();
 
-// app.use(cors("*"));
+const app = express();
 
 app.use(express.json());
 
