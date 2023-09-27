124c7f24990b78be43c8e8132944d7e6c4d78107
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-09-27 10:44:19 -0500

    removing json middleware for testing

--- index.js
+++ index.js
@@ -7,42 +7,47 @@ config();
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("up on /");
 });
 
-app.post("/wrike", header("X-Hook-Secret").notEmpty(), (req, res, next) => {
-  const wrikeHookSecret = process.env.wrikeHookSecret;
-  const errors = validationResult(req).errors;
-  if (errors.length === 0) {
-    const xHookSecret = req.get("X-Hook-Secret");
-    if (req.body["requestType"] === "WebHook secret verification") {
-      const calculatedHash = crypto
-        .createHmac("sha256", wrikeHookSecret)
-        .update(xHookSecret)
-        .digest("hex");
-      // Change
-      console.log(calculatedHash, xHookSecret);
-      res.status(200).set("X-Hook-Secret", calculatedHash).send();
+app.post(
+  "/wrike",
+  header("X-Hook-Secret").notEmpty(),
+  async (req, res, next) => {
+    const wrikeHookSecret = process.env.wrikeHookSecret;
+    const errors = validationResult(req).errors;
+    const jsonBody = await req.body.json();
+    if (errors.length === 0) {
+      const xHookSecret = req.get("X-Hook-Secret");
+      if (jsonBody["requestType"] === "WebHook secret verification") {
+        const calculatedHash = crypto
+          .createHmac("sha256", wrikeHookSecret)
+          .update(xHookSecret)
+          .digest("hex");
+        // Change
+        console.log(calculatedHash, xHookSecret);
+        res.status(200).set("X-Hook-Secret", calculatedHash).send();
+      } else {
+        const calculatedHash = crypto
+          .createHmac("sha256", wrikeHookSecret)
+          .update(req.body)
+          .digest("hex");
+        // console.log(calculatedHash, xHookSecret);
+        console.log(
+          // req.body,
+          `My hash: ${calculatedHash} \n Wrike hash ${xHookSecret}`
+        );
+        // wrikeRouting(req.body);
+        res.status(200).send("good");
+      }
     } else {
-      const calculatedHash = crypto
-        .createHmac("sha256", wrikeHookSecret)
-        .update(JSON.stringify(req.body))
-        .digest("hex");
-      // console.log(calculatedHash, xHookSecret);
-      console.log(
-        req.body,
-        `My hash: ${calculatedHash} \n Wrike hash ${xHookSecret}`
-      );
-      wrikeRouting(req.body);
-      res.status(200).send("good");
+      res.status(400).send("Incorrect Header");
     }
-  } else {
-    res.status(400).send("Incorrect Header");
   }
-});
+);
 
 app.post("/graph", (req, res) => {
   const graphClientSecret = process.env.graphClientSecret;
