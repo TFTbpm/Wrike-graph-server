034e06ec5f363d400c5c3a1f4bb8d3074b094f49
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-09-27 11:04:27 -0500

    Take in raw request steam for crypto

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ const wrikeRouting = require("./modules/wrike/wrikeRouting");
 config();
 
 const app = express();
+let rawRequestBody = "";
+
+app.post("/wrike", (req, res, next) => {
+  req.on("data", (chunk) => {
+    rawRequestBody += chunk;
+    console.log(rawRequestBody);
+  });
+  next();
+});
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("up on /");
-});
-
 app.post("/wrike", header("X-Hook-Secret").notEmpty(), (req, res, next) => {
-  const wrikeHookSecret = process.env.wrikeHookSecret;
+  const wrikeHookSecret = process.env.wrike_hook_secret;
   const errors = validationResult(req).errors;
   if (errors.length === 0) {
     const xHookSecret = req.get("X-Hook-Secret");
@@ -29,19 +34,21 @@ app.post("/wrike", header("X-Hook-Secret").notEmpty(), (req, res, next) => {
     } else {
       const calculatedHash = crypto
         .createHmac("sha256", wrikeHookSecret)
-        .update(JSON.stringify(req.body))
+        .update(rawRequestBody)
         .digest("hex");
-      // console.log(calculatedHash, xHookSecret);
       console.log(
         req.body,
         `My hash: ${calculatedHash} \n Wrike hash ${xHookSecret}`
       );
-      wrikeRouting(req.body);
       res.status(200).send("good");
     }
   } else {
     res.status(400).send("Incorrect Header");
   }
+});
+
+app.get("/", (req, res) => {
+  res.send("up on /");
 });
 
 app.post("/graph", (req, res) => {
