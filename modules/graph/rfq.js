3c78edcb3f37acc089e8b4c8eaa58f784d9aa368
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-10-16 15:16:44 -0500

    remove token log

--- modules/graph/rfq.js
+++ modules/graph/rfq.js
@@ -1,5 +1,4 @@
 async function getRFQData(site_id, list_id, access_token) {
-  console.log(access_token);
   const url = `https://graph.microsoft.com/v1.0/sites/${site_id}/lists/${list_id}/items?filter=contentType/name eq 'Request for Quote'&expand=fields&orderby=fields/Modified%20desc&top=5`;
   const requestOptions = {
     headers: {
