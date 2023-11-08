fc1ac8805f9bcefe5764e46b452441d364ac194e
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-11-08 08:15:07 -0600

    got a very slow order function created

--- modules/graph/order.js
+++ modules/graph/order.js
@@ -1,4 +1,5 @@
 async function getOrders(site_id, list_id, access_token) {
+  const startTime = performance.now();
   const url = `https://graph.microsoft.com/v1.0/sites/${site_id}/lists/${list_id}/items`;
   const requestOptions = {
     headers: {
@@ -6,10 +7,28 @@ async function getOrders(site_id, list_id, access_token) {
       Prefer: "HonorNonIndexedQueriesWarningMayFailRandomly",
     },
   };
-  console.log(url);
-  //   const response = await fetch(url, requestOptions);
-  //   const data = await response.json();
-  //   return await data;
+  let response = await fetch(url, requestOptions);
+  let data = await response.json();
+  let allItems = [];
+  let page = 1;
+  while (data["@odata.nextLink"]) {
+    response = await fetch(data["@odata.nextLink"], requestOptions);
+    data = await response.json();
+    // 37 seems arbitrary, but there are over 7700 entries here so sorting all of them will be a huge waste
+    if (page > 37) {
+      allItems.push(...data.value);
+    }
+    page++;
+    console.log(page);
+  }
+  allItems.sort(
+    (a, b) =>
+      new Date(b.lastModifiedDateTime) - new Date(a.lastModifiedDateTime)
+  );
+  const filteredItems = allItems.slice(0, 5);
+  const endTime = performance.now();
+  console.log(`time taken: ${endTime - startTime}`);
+  return filteredItems;
 }
 
 module.exports = getOrders;
