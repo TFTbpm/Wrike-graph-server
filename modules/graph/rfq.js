4048e483097e5545ed868a2e29bbf8700e0217fe
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-11-10 16:36:58 -0600

    added logging for what's being sent to system

--- modules/graph/rfq.js
+++ modules/graph/rfq.js
@@ -62,6 +62,12 @@ async function modifyGraphRFQ(hooks, graphIDToWrikeID) {
           continue;
         }
 
+        console.log({
+          resource: "RFQ",
+          assignee: assignee.name,
+          id: parseInt(mongoEntry.graphID),
+          type: "REMOVE",
+        });
         // send data to power automate
         try {
           const response = await fetch(process.env.graph_power_automate_uri, {
@@ -104,6 +110,13 @@ async function modifyGraphRFQ(hooks, graphIDToWrikeID) {
           console.log(`id is not stored! ID: ${hook.removedResponsibles}`);
           continue;
         }
+
+        console.log({
+          resource: "RFQ",
+          assignee: assignee.name,
+          id: parseInt(mongoEntry.graphID),
+          type: "REMOVE",
+        });
 
         try {
           const response = await fetch(process.env.graph_power_automate_uri, {
