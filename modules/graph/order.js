859b8450cd5d1aa3d5c1c55e757c249b3b08d250
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-11-20 15:10:49 -0600

    troubleshooting graph url

--- modules/graph/order.js
+++ modules/graph/order.js
@@ -54,7 +54,7 @@ async function getOrders(site_id, list_id, access_token, skipToken) {
 
 async function addOrder(data, name, uri, metaData) {
   try {
-    const response = await fetch(uri, {
+    fetch(uri, {
       method: "PATCH",
       body: JSON.stringify({
         resource: "Order",
@@ -67,11 +67,7 @@ async function addOrder(data, name, uri, metaData) {
         "Content-Type": "application/json",
       },
     });
-    if (!response.ok) {
-      console.log(
-        `there was an issue with the fetch request: ${response.statusText} \n ${response.status}`
-      );
-    }
+    console.log("sent");
     // console.log(await response.text());
     // const result = await response.json();
 
